4e21546af02058ab2c3b90155db00aa55d7b672a
Backend <dhundhoobackend@gmail.com>
2024-08-26 14:21:34 +0530

    1

---
+++ src/test/java/com/pro/maventest/MaventestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.maventest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaventestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
